d1d1bfa3c3ec385ce04a00443c28d8a99796fd6e
Milan Radanovic <milan.radanovic@sprintingsoftware.com>
2023-11-13 21:41:03 +0100

    Shitty tutorial, missing code so this ain't working but at least I listened to the theory. Really, really bad tutorial.

---
+++ TryingOutBlazor/FirstBlazorApp/Controllers/ForecastController.cs
@@ -0,0 +1,16 @@
+﻿using FirstBlazorApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstBlazorApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ForecastController: ControllerBase
+    {
+        public async Task<ActionResult<IEnumerable<WeatherForecast>>> GetAction([FromServices] WeatherForecastService service, DateOnly startDate)
+        {
+            var svc = new WeatherForecastService();
+            return new OkObjectResult(await svc.GetForecastAsync(startDate));
+        }
+    }
+}
